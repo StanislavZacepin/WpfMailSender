d96410dfd67da2e52460f211f31c3c63d23ecc39
TouchPad <olsher258@mail.ru>
2021-08-25 08:51:30 +0300

    Создание тест проектов

---
+++ TestConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestConsole
+{
+    class Program
+    {
+        // Для провеки кода для отправки письма 
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- TestWpfMailSender/Views/MainWindow.xaml.cs
+++ TestWpfMailSender/Views/MainWindow.xaml.cs
@@ -20,6 +20,7 @@ namespace TestWpfMailSender
     /// </summary>
     public partial class MainWindow : Window
     {
+        //Главное окно
         public MainWindow()
         {
             InitializeComponent();
