a4f46986f344248549dd15f0dfc386b31fd8d3d3
TouchPad <olsher258@mail.ru>
2021-08-29 13:46:29 +0300

    Создания Классов

---
+++ TestWpfMailSender/Models/Server.cs
@@ -0,0 +1,16 @@
+﻿namespace TestWpfMailSender.Models
+{
+    public class Server
+    {
+        public string Name { get; set; }
+        public string Addres { get; set; }
+
+        public int Port { get; set; }
+
+        public bool UseSSL { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+    }
+}
