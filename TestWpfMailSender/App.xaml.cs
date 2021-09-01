68d26874bb2e335dcc9c6db50a0182f718559001
TouchPad <olsher258@mail.ru>
2021-09-01 10:43:33 +0300

    Контенер сервисов

--- TestWpfMailSender/App.xaml.cs
+++ TestWpfMailSender/App.xaml.cs
@@ -5,6 +5,10 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using TestWpfMailSender.ViewModels;
 
 namespace TestWpfMailSender
 {
@@ -13,5 +17,21 @@ namespace TestWpfMailSender
     /// </summary>
     public partial class App : Application
     {
+        private static IHost __Hosting;
+
+        public static IHost Hosting => __Hosting
+            ??= CreatHostBulder(Environment.GetCommandLineArgs()).Build();
+
+        public static IServiceProvider Services => Hosting.Services;
+
+        public static IHostBuilder CreatHostBulder(string[] args) => 
+            Host.CreateDefaultBuilder(args).ConfigureAppConfiguration(opt => opt.AddJsonFile("appsettings.json",false, true))
+            .ConfigureServices(ConfigureServices);
+
+        private static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddSingleton<MainWindowViewModel>();
+        }
+        
     }
 }
