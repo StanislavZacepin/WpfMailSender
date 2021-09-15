eee94a6aa04747503be9be59af939d5f6b75540b
TouchPad <olsher258@mail.ru>
2021-09-15 14:15:14 +0300

    Автоматизция выполнения команд при загрузке приложения

--- TestWpfMailSender/Controls/RecipientEditor.xaml.cs
+++ TestWpfMailSender/Controls/RecipientEditor.xaml.cs
@@ -24,5 +24,17 @@ namespace TestWpfMailSender.Controls
         {
             InitializeComponent();
         }
+
+        private void OnNameValidationError(object Sender, ValidationErrorEventArgs E)
+        {
+            if(E.Action == ValidationErrorEventAction.Added)
+            {
+                ((Control)Sender).ToolTip = E.Error.ErrorContent.ToString();
+            }
+            else
+            {
+                ((Control)Sender).ClearValue(ToolTipProperty );
+            }
+        }
     }
 }
